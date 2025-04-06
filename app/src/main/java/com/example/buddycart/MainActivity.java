a43d37da7320174d80b9f66f3b65cca9ce33a2b5
Shlok Shah <106793433+shahshlok@users.noreply.github.com>
2025-04-06 15:20:41 -0700

    Remove Firebase dependencies and initialization from MainActivity and build.gradle.kts

--- app/src/main/java/com/example/buddycart/MainActivity.java
+++ app/src/main/java/com/example/buddycart/MainActivity.java
@@ -11,9 +11,6 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-
 public class MainActivity extends AppCompatActivity {
     private DatabaseReference mDatabase;
     private Button btnGoToShoppingCart;
@@ -29,12 +26,6 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        // Initialize Firebase Database
-        mDatabase = FirebaseDatabase.getInstance().getReference();
-
-        // Write a test message to the database
-        mDatabase.child("test").setValue("Hello, Firebase! again");
 
         // Initialize the buttons
         btnGoToShoppingCart = findViewById(R.id.btnGoToShoppingCart);
